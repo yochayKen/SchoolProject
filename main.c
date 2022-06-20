0f205da63ee9ed037b4273f589009ed3b801437d
YochayKen <yochay95@gmail.com>
2022-06-20 12:27:51 -0700

    Add validation of the number of minimum files to get as arguments

--- main.c
+++ main.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc,char *argv[])
+#define MIN_NUM_OF_FILES 2
+
+int validate_num_of_arguments(int, char *);
+
+int main(int argc, char *argv[])
 {
-    FILE *fptr;
-    char buffer[1000];
+    char *original_file_content;
+    int state_flag = 0;
 
-    fptr = fopen("/tests/table_test.py", "r");
-    fread(buffer, 1000, 1, fptr);
-    printf("%s\n", buffer);
+    if (validate_num_of_arguments(argc, argv[0]) == 0)
+        return 1;
+
     
-
+    
     return 0;
+}
+
+int validate_num_of_arguments(int num_of_args, char *arguments)
+{
+    if (num_of_args < MIN_NUM_OF_FILES)
+    {
+        printf("error: insufficiant number of files\nUsage: %s <file_path_1> <file_path_2> ... <file_path_N>\n", arguments);
+        return 0;
+    }
+    return 1;
 }
