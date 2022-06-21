a6dfbe49ad4b24dbae77e7d2432feb59ef6717df
YochayKen <yochay95@gmail.com>
2022-06-21 10:00:32 -0700

    Checking create and append to list

--- list.c
+++ list.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
 
 /*Create a generic linked list. Return a List object.*/
-void *create_list(void *node)
+List *create_list()
+{
+    List *list;
+    list = (List *)malloc(sizeof(List));
+    list->head = list->tail = NULL;
+    list->length = 0;
+    return list;
+}
+
+void append_node(List *list, void *data)
+{
+    Node *node, *node_ptr;
+    node = (Node *)malloc(sizeof(Node));
+    node->data = data;
+    if (list->head == NULL)
+    {
+        list->head = list->tail = node;
+    }
+    else
+    {
+        node_ptr = list->tail;
+        node_ptr->priv = list->tail->priv;
+        list->tail = node;
+        node_ptr->next = list->tail;
+        list->tail = NULL;
+    }
+    list->length++;
+}
+
+void remove_node(List *list, void *data)
 {
     return;
 }
 
-void append(void *list, void *node)
-{
-    return;
-}
-
-void remove(void *list, void *node)
-{
-    return;
-}
-
-void delete_list(void *list)
+void delete_list(List *list)
 {
     return;
 }
--- list.h
+++ list.h
@@ -1,10 +1,21 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+typedef struct node{
+    void *data;
+    struct node *next;
+    struct node *priv;
+}Node;
 typedef struct list{
-    void *head;
-    void *tail;
+    Node *head;
+    Node *tail;
     unsigned int length;
 }List;
+
+/* API */
+List *create_list(void);
+void append_node(List *, void *);
+void remove_node(List *, void *);
+void delete_list(List *);
 
 #endif
